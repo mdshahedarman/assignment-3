9ce4ab1ca06207d3d81a57e6a3c385a1e07f079d
mdshahedarman <m.s.arman4244@gmail.com>
2021-01-22 22:05:46 +0600

    work done

--- assignment.js
+++ assignment.js
@@ -1,4 +1,24 @@
-function kilometerToMeter (kilo){
-    var meter = kilo * 1000
-    return meter;
+//function kilomeeterToMeter
+function kilomeeterToMeter(kilometer){
+    var meter = kilometer * 1000 ;
+    return meter ;
 }
+var data = [2 ,3, 5]
+var kiloMeter = kilometerToMeter(data[0]);
+console.log(kiloMeter);
+
+
+//function budgetCalculator
+function budgetCalculator (watch, phone, laptop){
+    var watchPrice = 50, phoneprice =100, laptopprice = 500;
+    if ( watch >= 0,  phone >= 0,  laptop >= 0){
+        return ( water * waterPrice) + (phone * phonePrice) + (laptop * laptopPrice);
+    }
+    else{
+        return"it has no value ";
+    }
+}
+
+
+
+//function hotelCost
